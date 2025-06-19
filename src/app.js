95a0911f8a7c68f3203148f0c7bb4dad4931ff6b
Harsh Raj <harshraj2652@gmail.com>
2025-06-20 03:00:24 +0530

    Explored GET, POST, DELETE routes Explored get/post/delete

--- src/app.js
+++ src/app.js
@@ -2,6 +2,18 @@ const express = require("express")
 
 const app = express();
 
+app.get("/user",(req,res)=>{
+    res.send({firstName:"Harsh" , lastName:"Raj"})
+})
+
+app.post("/user",(req,res)=>{
+    res.send("User successfully add to DB")
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("User Deleted from DB")
+})
+
 app.use("/test",(req,res)=>{
     res.send("Hello form the Server hahahahhshs")
 })
