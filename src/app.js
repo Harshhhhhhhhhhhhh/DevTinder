c85d05b5651c0d7b77b2d4d21efbbec605a7a52a
Harsh Raj <126498672+Harshhhhhhhhhhhhh@users.noreply.github.com>
2025-06-21 03:43:26 +0530

    Dummy Error Handling

--- src/app.js
+++ src/app.js
@@ -2,21 +2,39 @@ const express = require("express")
 
 const app = express();
 
-const {adminAuth} = require("./middlewares/auth.js")
 
-app.use("/admin",adminAuth);
+ app.use("/",(err,req,res,next)=>{
+     if(err){
+         res.status(500).send("Something went wrong")
+     }
 
-app.get("/admin/getAllData",(req,res)=>{
+ })
 
-    res.send("Data sent");
 
+app.get("/getUserData",(req,res)=>{
+
+    //try{
+        //logic for DataBase call and get user data
+        throw new Error("Throwing randome error")
+        res.send("User Data is sent");
+
+    //}
+    // catch(err){
+    //     res.status(500).send("Throwing randome error")
+
+    // }
 
 }
 );
-app.delete("/admin/deleteData",(req,res)=>{
-    
-    res.send("Data deleted")
-})
+
+
+ app.use("/",(err,req,res,next)=>{
+     if(err){
+         res.status(500).send("Something went wrong")
+     }
+
+ })
+
 
 app.listen(3000,()=>{
     console.log("Server is successfully running on port 3000")
