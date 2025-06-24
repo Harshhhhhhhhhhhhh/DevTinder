1b74fea0ad6cda0c7ee662fa1819c174dc7c87c5
Harsh Raj <126498672+Harshhhhhhhhhhhhh@users.noreply.github.com>
2025-06-24 11:30:47 +0530

    get/user and get/feed-->all users

--- src/app.js
+++ src/app.js
@@ -24,6 +24,44 @@ app.post("/signUp" ,async (req,res)=>{
 
 })
 
+app.get("/user",async(req,res)=>{
+    const userEmail = req.body.emailId;
+    try{
+        const user = await User.find({emailId:userEmail});
+        if(user.length===0){
+            res.status(400).send("Cannot find user")
+
+        }
+        else{
+            res.send(user)
+        }
+    }
+    catch(err){
+        res.status(400).send("Something went wrong "+err)
+    }
+
+})
+
+//Feed-API--get/feed--get all users from data base
+
+app.get("/feed",async (req,res)=>{
+    try{
+        const users = await User.find({});
+        // if(users.length===0){
+        //     res.status(400).send("Cannot find user")
+
+        // }
+        //else{
+            res.send(users)
+        //}
+    }
+    catch(err){
+        res.status(400).send("Something went wrong "+err)
+    }
+    
+
+})
+
 
 
 connectDB()
