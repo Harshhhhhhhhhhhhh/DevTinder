8a78ac1ca24b4d3ce5bdf9efbdf7b0e5543bf3b6
Harsh Raj <126498672+Harshhhhhhhhhhhhh@users.noreply.github.com>
2025-07-18 19:06:52 +0530

    Updated dB pasword

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose =require("mongoose")
 
 const connectDB = async (params) => {
     await mongoose.connect(
-    "mongodb+srv://harshraj2651:9006631511harsh@cluster0.yevlgkg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    "mongodb+srv://harshraj2651:8QtXCMw4YYcyN38v@cluster0.yevlgkg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     )    
 }
 
